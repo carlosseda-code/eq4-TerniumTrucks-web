18dec891d0ec2ce84e43a9f7e58f91c45fc33376
Carlos Seda <carlosseda123@gmail.com>
2021-03-03 22:20:37 -0600

    commit cs

--- ttrucks/src/App.js
+++ ttrucks/src/App.js
@@ -5,11 +5,7 @@ function App() {
   return (
     <div className="App">
      <h2> lol </h2>
-<<<<<<< HEAD
      <h3>CAMBIO CS</h3>
-=======
-     <h3> hola soy santi, prueba para ver si si jalan los pushes</h3>
->>>>>>> 25895497aa32d7cd6eed6044a3ff896dd3fa9301
     </div>
   );
 }
